a0d1f435d2e20c1b187cd9dbb6ea609856cb4b1a
Flying Birds <giantpanda968@gmail.com>
2026-09-08 22:57:06 +0530

    Fix Cors or Production

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,10 @@ app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://fullstack-chat-app-wyak.onrender.com",
+    ],
     credentials: true,
   }),
 );
